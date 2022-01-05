7e34904d700fbf9825034d00883f13e77434ee36
andre170594 <andre_pessoa99@hotmail.com>
2022-01-05 19:07:19 +0000

    updateMenu

--- src/tpgps/controllers/MenuController.java
+++ src/tpgps/controllers/MenuController.java
@@ -3,6 +3,9 @@ package tpgps.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import tpgps.DbaseUtils;
+import tpgps.model.User;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,10 +16,18 @@ public class MenuController implements Initializable {
     @FXML Button btn_removeReserva;
     @FXML Button btn_verReservas;
     @FXML Button btn_verLugares;
+    @FXML Label txt_nomeAl;
+
+    User alumni;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-            // fazer setOn action para cada Um
+           alumni = DbaseUtils.getAlumni();
+
+           //DEBUG
+        System.out.println( "Aluno: " + alumni.getNameUser());
+
+           txt_nomeAl.setText(alumni.getNameUser());
 
 
     }
