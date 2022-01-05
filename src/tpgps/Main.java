ea658de961f0a3e816321fb0f5d2d0f6e6b023bc
andre170594 <andre_pessoa99@hotmail.com>
2022-01-05 11:52:04 +0000

    logIn concluido

--- src/tpgps/Main.java
+++ src/tpgps/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/LogIn.fxml"));
         primaryStage.setTitle("BookingMasters");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
