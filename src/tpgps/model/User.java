06280da2070b4a549670fb0a1e3092c1ab7ddeff
andre170594 <andre_pessoa99@hotmail.com>
2022-01-05 19:15:47 +0000

    intro BD isec

--- src/tpgps/model/User.java
+++ src/tpgps/model/User.java
@@ -2,36 +2,37 @@ package tpgps.model;
 
 public class User {
 
+    private int id;
     private String numUser;
     private String pwdUser;
     private String nameUser;
 
-    public User(String numUser, String pwdUser, String nameUser) {
+    public User(int id, String numUser, String pwdUser, String nameUser) {
+        this.id = id;
         this.numUser = numUser;
         this.pwdUser = pwdUser;
         this.nameUser = nameUser;
     }
 
+
+    // GETS
+    public int getId() { return id; }
     public String getNumUser() {
         return numUser;
     }
-
-    public void setNumUser(String numUser) {
-        this.numUser = numUser;
-    }
-
-    public String getPwdUser() {
-        return pwdUser;
-    }
-
-    public void setPwdUser(String pwdUser) {
-        this.pwdUser = pwdUser;
-    }
-
+    private String getPwdUser(){ return pwdUser;}
     public String getNameUser() {
         return nameUser;
     }
 
+    // SETS
+    public void setId(int id) { this.id = id; }
+    public void setNumUser(String numUser) {
+        this.numUser = numUser;
+    }
+    public void setPwdUser(String pwdUser) {
+        this.pwdUser = pwdUser;
+    }
     public void setNameUser(String nameUser) {
         this.nameUser = nameUser;
     }
