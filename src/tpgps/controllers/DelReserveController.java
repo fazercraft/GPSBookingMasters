1d7d6cdcd83e2438dc937ff397e004636e5851d6
andre170594 <andre_pessoa99@hotmail.com>
2022-01-05 17:07:18 +0000

    Login | menu | ReserveSeat | RemoveSeat layouts feitos.
    No Login falta fazer o ficheiro de texto que representa a bd

---
+++ src/tpgps/controllers/DelReserveController.java
@@ -0,0 +1,14 @@
+package tpgps.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DelReserveController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
